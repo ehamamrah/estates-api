1ef0014f5ee94397a7e827be1d1687d3db2d76cc
Elayan Hamamrah <Elayan.hr@gmail.com>
2018-10-08 00:49:16 +0300

    Finished Inital Estates Controller

--- app/controllers/api/v1/estates_controller.rb
+++ app/controllers/api/v1/estates_controller.rb
@@ -1,6 +1,48 @@
 module Api
   module V1
     class EstatesController < ApplicationController
+      before_action :find_real_estate, except: %i[index create]
+
+      def index
+        estates = Estates.order('created_at DESC')
+        render json: { status: 'Success', message: 'Estates List', data: estates }
+      end
+
+      def create
+        estate = Estate.new(estate_params)
+        estate.save ? success_response(estate) : failure_response(estate)
+      end
+
+      def update
+        estate.update_attributes(estate_params) ? success_response(estate) : failure_response(estate)
+      end
+
+      def destroy
+        estate.destroy
+        render json: { status: 'Success', message: 'Removed real estate from our list' }
+      end
+
+      private
+
+      def find_real_estate
+        estate = Estate.find(params[:id])
+      end
+
+      def estate_params
+        params.require(:estate).permit(real_estate_fields)
+      end
+
+      def real_estate_fields
+        %i[street city zip state beds baths sq_ft residential_type sale_date price latitude longitude]
+      end
+
+      def success_response(estate)
+        render json: { status: 'Success', message: 'Success Loading Of Real Estate', data: estate }
+      end
+
+      def failure_response(estate)
+        render json: { status: 'Failed', message: 'Something Went Wrong'}
+      end
     end
   end
 end
