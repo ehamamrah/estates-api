657d7ac475909246fafb35110c837183455baf5b
Elayan Hamamrah <Elayan.hr@gmail.com>
2018-10-08 01:26:53 +0300

    Added Validations

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -1,3 +1,5 @@
 class Estate < ApplicationRecord
+  validates_presence_of :street, :city, :zip, :state, :beds, :baths, :sq_ft, :residential_type, :sale_date, :price, :latitude, :longitude
+
   scope :ordered_by_creation_date, -> { order('created_at DESC') }
 end
