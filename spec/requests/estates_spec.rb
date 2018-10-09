744fb99cddf64544c897957282cce60339dcc686
Elayan Hamamrah <Elayan.hr@gmail.com>
2018-10-09 23:25:30 +0300

    Show Action API Specs

--- spec/requests/estates_spec.rb
+++ spec/requests/estates_spec.rb
@@ -75,4 +75,27 @@ RSpec.describe 'Estates', type: :request do
       expect(search_response['data'].count).to eql(5)
     end
   end
+
+  describe '#Show' do
+    it 'will show specific real estate details' do
+      record = Estate.create!(street:           Faker::Address.street_name,
+                              city:             Faker::Address.city,
+                              zip:              Faker::Address.zip,
+                              state:            Faker::Address.state,
+                              beds:             [1, 2, 3].sample,
+                              baths:            [1, 2].sample,
+                              longitude:        Faker::Address.longitude,
+                              latitude:         Faker::Address.latitude,
+                              residential_type: ['Residential', 'Condo', 'Multi-Family'].sample,
+                              sale_date:        Date.today - 10.days,
+                              price:            [59_000.0, 66_000.0, 75_000.0].sample,
+                              sq_ft:            [950, 1500, 1250, 754].sample)
+
+      get "#{api}/estates/#{record.id}"
+      expect(response).to have_http_status(200)
+      json_response = JSON.parse(response.body)
+      record_data   = json_response['data'].first
+      expect(record_data['id']).to eql(record.id)
+    end
+  end
 end
